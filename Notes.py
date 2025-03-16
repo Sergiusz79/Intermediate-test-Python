cccbcffae815b0ddcc23aab3870b8dbf95ac229f
Sergiusz79 <mc.sergey30@gmail.com>
2025-03-16 14:45:07 +0100

    class Note

--- Notes.py
+++ Notes.py
@@ -3,7 +3,31 @@ import csv
 from datetime import datetime
 
 class Note:
+    def __init__(self, note_id, title, body, timestamp=None):
+        self.note_id = note_id
+        self.title = title
+        self.body = body
+        self.timestamp = timestamp or datetime.now().isoformat()
 
- class NoteManager:
+    def update(self, title=None, body=None):
+        if title:
+            self.title = title
+        if body:
+            self.body = body
+        self.timestamp = datetime.now().isoformat()
+
+    def to_dict(self):
+        return {
+            "id": self.note_id,
+            "title": self.title,
+            "body": self.body,
+            "timestamp": self.timestamp
+        }
+    
+    @staticmethod
+    def from_dict(data):
+        return Note(data["id"], data["title"], data["body"], data["timestamp"])
+
+class NoteManager:
 
 def main(): 
